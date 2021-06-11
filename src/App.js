ea32b63c74481d67f0522da88e31c1e708126bab
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-06-11 23:21:31 +0200

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Angelo1 } from './Angelo1';
 import { Angelo2 } from './components/Angelo2';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  HashRouter,
+  HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 export const App = () => {
   return (
-    <Router>
+    <HashRouter hashType="noslash">
       <Switch>
         <Route exact path="/world4-level2" component={Angelo1} />
         <Route path="/world4-level2/Angelo2" component={Angelo2} />
         <Route component={() => <h1>This page doesn't exist!! </h1>} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 };
