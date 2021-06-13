9373071886819d96816fd2c4ea8df3d9262d6504
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-06-13 23:14:42 +0200

    changed  values to react-sound

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Angelo1 } from './Angelo1';
 import { Angelo2 } from './components/Angelo2';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Sound from 'react-sound';
-import Music from 'music.mp3';
-
-
+import Music from './images/music.mp3';
 
 export const App = () => {
   return (
@@ -14,8 +12,13 @@ export const App = () => {
         <Route exact path="/world4-level2" component={Angelo1} />
         <Route path="/" component={Angelo2} />
         <Route component={() => <h1>This page doesn't exist!! </h1>} />
+        <Sound
+          url={Music}
+          playStatus={Sound.status.PLAYING}
+          playFromPosition={300}
+          loop={true}
+        />
       </Switch>
     </Router>
-    <Sound url={Music} playStatus={Sound.status.PLAYING} loop={true} />
   );
 };
