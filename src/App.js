6069ac6dd2f8cf778b1fec600838f1034dd52606
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-06-13 23:16:03 +0200

    change back

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Angelo1 } from './Angelo1';
 import { Angelo2 } from './components/Angelo2';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Sound from 'react-sound';
-import Music from './images/music.mp3';
 
 export const App = () => {
   return (
@@ -12,12 +10,6 @@ export const App = () => {
         <Route exact path="/world4-level2" component={Angelo1} />
         <Route path="/" component={Angelo2} />
         <Route component={() => <h1>This page doesn't exist!! </h1>} />
-        <Sound
-          url={Music}
-          playStatus={Sound.status.PLAYING}
-          playFromPosition={300}
-          loop={true}
-        />
       </Switch>
     </Router>
   );
